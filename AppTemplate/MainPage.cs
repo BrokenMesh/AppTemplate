2e3d32dc6042ce0d0020cc81999a1ba66b774a25
BrokenMesh <mineking61442@gmail.com>
2024-10-23 22:04:43 +0200

    MainPage demo

--- AppTemplate/MainPage.cs
+++ AppTemplate/MainPage.cs
@@ -3,14 +3,53 @@ namespace AppTemplate;
 public sealed partial class MainPage : Page
 {
     public MainPage() {
-        this
-            .Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"))
-            .Content(new StackPanel()
-            .VerticalAlignment(VerticalAlignment.Center)
-            .HorizontalAlignment(HorizontalAlignment.Center)
-            .Children(
-                new TextBlock()
-                    .Text("Hello Uno Platform!")
-            ));
+        this.DataContext(new MainViewModel(), Render);
+    }
+
+    public void Render(MainPage _page, MainViewModel _vm) {
+        _page.Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"));
+        _page.Content(new StackPanel {
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+            }.Children(
+                new TextBlock {
+                    Margin = new Thickness(12),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    TextAlignment = Microsoft.UI.Xaml.TextAlignment.Center,
+                }.Text("Hello Uno Platform!"),
+
+                new TextBlock {
+                    Margin = new Thickness(12),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    TextAlignment = Microsoft.UI.Xaml.TextAlignment.Center,
+                }.Text(() => _vm.Count, (txt) => $"Counter: {txt}"),
+
+                new NumberBox {
+                    Margin = new Thickness(12),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    TextAlignment = Microsoft.UI.Xaml.TextAlignment.Center,
+                }.Text(x => x.Binding(() => _vm.Step).TwoWay()),
+
+                new Button {
+                    Margin = new Thickness(12),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Content = "Increment"
+                }.Command(() => _vm.IncrementCommand)
+            )
+        );
+    }
+
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int _step = 1;
+
+        [ObservableProperty]
+        private int _count = 0;
+
+        [RelayCommand]
+        private void Increment() {
+            Count += Step;
+        }
     }
 }
